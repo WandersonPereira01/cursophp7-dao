d39ee378a2d8980756490ffb1ce9c87558e9853a
Wanderson <wanderson.p.ayres@gmail.com>
2019-06-15 16:24:45 -0300

    Criado os métodos de busca de usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -62,6 +62,49 @@ class Usuario{
 
 	}
 
+	//Método estático para listar todos os usuários da tabela
+	public static function getLista(){
+		
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	//Método estático para procurar um registro no banco pelo login
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH' => "%".$login."%"  //a porcentagem insere as aspas simples no banco, evitando o SQL Injection
+ 		));
+	}
+
+	//Método para trazer usuários autenticados, ou seja, tem que informar o login e senha
+	public function login($login, $senha){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(
+			":LOGIN" => $login,
+			":SENHA" => $senha
+		));
+
+		//Verificando se a consulta retornou dados
+		if (count($results) > 0){
+			//variável que recebe o valor de $results na posição ZERO
+			$row = $results[0];
+
+			//pegando os dados que voltaram associativos e mandando para os setters
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+			throw new Exception("Login e/ou senha inválidos.");
+			
+		}
+
+	}
+
 	//Método para trazer as informações do banco em String usando o método mágico __toString()	
 	public function __toString(){
 		return json_encode(array(
--- index.php
+++ index.php
@@ -2,8 +2,27 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega 1 usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
 
-echo $root;
+//carrega uma lista de usuários
+//Como é um método estático, não precisa ser instanciado
+//$lista = Usuario::getLista();
+//echo json_encode($lista);
+
+
+//carrega a lista de usuários buscando pelo login
+//$busca = Usuario::search("ma"); // vai retornar todos os usuarios que começam com 'ma'
+//echo json_encode($busca);
+
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Manolo", "qwert");
+
+echo $usuario;
+
+
